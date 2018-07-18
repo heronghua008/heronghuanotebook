9daee7c8ccc6bfb9e48c85c81deec11cf284a537
heronghua008 <heronghua008@live.com>
2018-07-18 12:35:01 +0800

    更新笔记

--- pythonNET/pythonNET02/client.py
+++ pythonNET/pythonNET02/client.py
@@ -1,7 +1,7 @@
 # client.py
 import socket
 sk = socket.socket()
-sk.connect(('127.0.0.1',8889))
+sk.connect(('0.0.0.0',8888))
 while True:
     info = input('>>>')
     sk.send(bytes(info,encoding='utf-8'))
